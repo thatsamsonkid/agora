3f5f1110bfd7bd78bb70d2ebdfb2ade28c478365
Sammy Mohamed <sammymohamed1223@gmail.com>
2024-03-28 00:23:46 -0400

    Initial Project setup complete

--- spar-tan-toe/src/app/pages/analog-welcome.component.ts
+++ spar-tan-toe/src/app/pages/analog-welcome.component.ts
@@ -4,7 +4,7 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { shareReplay, Subject, switchMap, take } from 'rxjs';
 import { waitFor } from '@analogjs/trpc';
 import { injectTrpcClient } from '../../trpc-client';
-import { Note } from '../../note';
+import { Note } from '../../db';
 
 @Component({
   selector: 'spar-tan-toe-analog-welcome',
@@ -17,10 +17,12 @@ import { Note } from '../../note';
   template: `
     <main class="flex-1 mx-auto">
       <section class="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-        <div
-          class="flex max-w-[64rem] flex-col items-center gap-4 text-center"
-        >
-          <img class="h-12 w-12" src="https://analogjs.org/img/logos/analog-logo.svg" alt="AnalogJs logo. Two red triangles and a white analog wave in front"/>
+        <div class="flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <img
+            class="h-12 w-12"
+            src="https://analogjs.org/img/logos/analog-logo.svg"
+            alt="AnalogJs logo. Two red triangles and a white analog wave in front"
+          />
           <a
             class="rounded-2xl bg-zinc-200 px-4 py-1.5 text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
             target="_blank"
@@ -61,9 +63,7 @@ import { Note } from '../../note';
           <h2 class="text-[#DD0031] font-medium text-3xl leading-[1.1]">
             Leave a note
           </h2>
-          <p
-            class="max-w-[85%] leading-normal sm:text-lg sm:leading-7"
-          >
+          <p class="max-w-[85%] leading-normal sm:text-lg sm:leading-7">
             This is an example of how you can use tRPC to superpower your
             client-server interaction.
           </p>
